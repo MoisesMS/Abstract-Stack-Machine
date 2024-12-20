ae99502c25e92565a8b77236e0c25c897294be9e
Moises <moisesmorentes@gmail.com>
2024-12-20 15:13:48 +0100

    init commit

---
+++ Compiler.py
@@ -0,0 +1,53 @@
+import argparse
+
+# ADD Añade un numero a la pila.
+# SUM Suma los dos últimos numeros de la pila. El resultado lo devuelve a la pila.
+# GET Obtiene el último número de la pila.
+
+# Pila de memoria
+stack = []
+
+def compiler(order):
+  if order == "EXIT":
+    exit()
+
+  if order.startswith("ADD"):
+    try:
+      number = int(order[4:])
+      stack.append(number)
+    except ValueError:
+      print("El dato insertado no es válido.")
+  elif order.startswith("GET"):
+    if len(stack) == 0:
+      print("La pila está vacía.")
+    else:
+      print(stack[-1])
+  elif order.startswith("SUM"):
+    if len(stack) < 2:
+      print("No hay suficientes elementos para sumar")
+    else:
+      sum = stack.pop() + stack.pop()
+      stack.append(sum)
+  else:
+    print("El comando no existe.")
+
+parser = argparse.ArgumentParser(description="Recibe un archivo por parametro")
+parser.add_argument("file", type=str, nargs="?", help="Archivo con el código")
+
+arg = parser.parse_args()
+    
+
+if arg.file:
+
+  if arg.file.endswith(".a"):
+    #Leer archivo
+    with open(arg.file, "r") as file:
+      lines = file.readlines()
+
+      for line in lines:
+        compiler(line)
+  else:
+    print("Archivo no reconocido")
+else:
+  while True:
+    compiler(input(">>> "))
